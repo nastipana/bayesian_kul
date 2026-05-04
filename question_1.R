d58a3e0faabe32d7637e0c0c4303ffe70c7f43e7
DayaLuna <94837691+DayaLuna@users.noreply.github.com>
2026-05-04 23:34:57 +0200

    splitting code into 2

--- question_1.R
+++ question_1.R
@@ -121,14 +121,6 @@ results1 <- coda.samples(
   thin = 1
 )
 
-#diagnostics
-traceplot(results1)
-gelman.diag(results1)
-gelman.plot(results1, ask = FALSE)
-effectiveSize(results1)
-acfplot(results1)
-summary(results1)
-
 
 
 #burn-in
@@ -141,14 +133,6 @@ results2 <- coda.samples(
   n.iter = 10000,
   thin = 3
 )
-
-#diagnostics
-traceplot(results2)
-gelman.diag(results2)
-gelman.plot(results2, ask = FALSE)
-effectiveSize(results2)
-acfplot(results2)
-summary(results2)
 
 
 
@@ -163,42 +147,6 @@ results3 <- coda.samples(
   thin = 1
 )
 
+
 # save the results so that the mcmc does not need to be computed repeatedly
 save.image(file = "mcmc_results.Rdata")
-
-#diagnostics
-traceplot(results3)
-gelman.diag(results3)
-gelman.plot(results3, ask = FALSE)
-effectiveSize(results3)
-densplot(results3[, "r"])
-acfplot(results3)
-summary(results3)
-
-
-# task 5 ########
-load("mcmc_results.Rdata")
-coef <- summary(results3)[[1]][,1] # the coefficient estimates as vector
-
-# 1: predicting the number of insurance claims for each age group in District 1,
-# Car Group 1, and 100 policyholders
-
-# district 1 and car group 1: baseline --> given by intercept beta0, meaning 
-# that all x_ji=0, for j=1,2,3,7,8,9.
-# holders = 100 = E_i
-# what we need to fluctuate: x_4i/x_5i/x_6i
-# E[Yi] = exp(log(Ei) + beta0 + beta1*x1i + beta2*x2i + ...)
-
-# prediction of number of claims for ...
-# ... age group <25: exp(log(100)-log_Holders_mean + beta_0)
-exp(log(100)-log_Holders_mean + coef[1])
-# ... age group 25-29: exp(log(100)-log_Holders_mean + beta_0 + beta_4*1)
-exp(log(100)-log_Holders_mean + coef[1] + coef[5])
-# ... age group 30-35: exp(log(100)-log_Holders_mean + beta_0 + beta_5*1)
-exp(log(100)-log_Holders_mean + coef[1] + coef[6])
-# ... age group >35: exp(log(100)-log_Holders_mean + beta_0 + beta_6*1)
-exp(log(100)-log_Holders_mean + coef[1] + coef[7])
-
-# 2: Give summary measures and plots of the posterior predictive distributions.
-
-
